43b40ee7d4832b80c508e03d2a3f014f59a0398d
GabrielRosa835 <gabriel.rosa.080305@gmail.com>
2025-10-22 19:14:59 -0300

    Initial

---
+++ src/test/java/com/example/ac2aw/Ac2AwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ac2aw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac2AwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
