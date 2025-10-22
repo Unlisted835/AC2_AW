feffd4f20d64bd75e57565c3ab06353175001c49
GabrielRosa835 <gabriel.rosa.080305@gmail.com>
2025-10-22 19:30:10 -0300

    Created Models

---
+++ src/main/java/com/example/ac2aw/models/Setor.java
@@ -0,0 +1,19 @@
+package com.example.ac2aw.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Setor {
+   private int id;
+   private String nome;
+
+   public Setor(String nome) {
+      this.nome = nome;
+   }
+}
